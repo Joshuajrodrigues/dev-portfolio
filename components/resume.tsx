db98bcb2e2e0bcbc79cc9ca19f2266f90e464136
Joshua <rodrigues.joshr@gmail.com>
2023-12-19 01:00:52 +0530

    resume added

--- components/resume.tsx
+++ components/resume.tsx
@@ -3,7 +3,13 @@ import Button from "./button";
 import { Download } from "lucide-react";
 export default function Resume() {
   return (
-    <Button onClick={() => {}}>
+    <Button onClick={() => {
+        let a = document.createElement("a");
+        a.href = "/resume/JoshuaRodrigues_resume_2023.pdf";
+        a.target = "_blank";
+        a.click();
+
+    }}>
       <span className="flex text-lg font-bolder">
         Resume <Download className="ml-2" />{" "}
       </span>
