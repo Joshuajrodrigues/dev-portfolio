f1fa334e2aeb58e3a318c39151486e59850651a1
Joshua <rodrigues.joshr@gmail.com>
2023-12-22 23:14:22 +0530

    career updated

--- components/career.tsx
+++ components/career.tsx
@@ -1,50 +1,28 @@
 import React from "react";
 import Tags from "./tags";
+import { ExternalLink } from "lucide-react";
 
 const Career = () => {
   return (
     <div>
       <section className="w-full flex flex-wrap">
+        <Tags>Completed computer engineering in 2021</Tags>
         <Tags>Over 2 years of industry experience</Tags>
-        <Tags>Currently working at Teknorix</Tags>
-        <Tags>Computer engineering degree</Tags>
-        <Tags>Mentorship of interns</Tags>
-        <Tags>Code reviews</Tags>
-        <Tags>Active team engagement</Tags>
-        <Tags>Interviewing potential candidates</Tags>
-        <Tags>Worked on CRM, Ticketing Systems, Document Recognition, Knowledge Base, News & Publications Management, E-commerce</Tags>
+        <Tags>
+          <a href="https://www.teknorix.com/" target="_blank">
+            <p className=" w-full">
+              Currently working with the lovely people at
+              <img
+                src="https://www.teknorix.com/wp-content/uploads/2019/01/teknorix-logomark.svg"
+                alt="Teknorix logo"
+                className="w-2 mx-1 inline"
+              />
+              <u>Teknorix Pvt Ltd </u> 
+              <ExternalLink size={8} className="mr-1 inline text-xs" />since 2021
+            </p>
+          </a>
+        </Tags>
       </section>
-      <p className="p-2">
-        With over two years of professional experience since commencing his
-        journey in 2021 after completing a degree in computer engineering,
-        Joshua joined Teknorix Pvt Ltd as a Software Developer specializing in
-        frontend technologies. Within this dynamic role, Joshua is responsible
-        for maintaining existing projects, implementing new frontend features to
-        meet client requirements, and proficiently resolving software bugs.
-      </p>
-      <p className="p-2">
-        Joshua's diverse skill set has been showcased in various projects,
-        including Customer Relationship Management (CRM), ticketing systems,
-        document recognition software, knowledge base platforms, news and
-        publications management, and e-commerce-related product cataloging and
-        inventory management. His adept contributions to these projects have not
-        only enriched his technical proficiency but have also positioned him as
-        a key collaborator in Teknorix's success.
-      </p>
-      <p className="p-2">
-        Beyond individual contributions, Joshua actively engages in mentoring
-        interns, sharing his knowledge and expertise to cultivate a
-        collaborative and skillful team. He plays a pivotal role in the
-        development process by conducting thorough code reviews, ensuring the
-        quality and efficiency of the software produced by Teknorix Pvt Ltd.
-        Additionally, Joshua contributes to the growth of the team by
-        interviewing potential candidates, leveraging his experience to identify
-        individuals who align with the company's vision and contribute to its
-        ongoing success. This commitment to mentorship, code review, talent
-        acquisition, and project excellence underscores Joshua's dedication to
-        fostering a culture of continuous learning and excellence within the
-        software development domain.
-      </p>
     </div>
   );
 };
