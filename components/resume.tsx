2aff77bddc8598498cddf547faaf9d950834dc4e
Joshua <rodrigues.joshr@gmail.com>
2023-12-23 02:20:23 +0530

    framer motion

--- components/resume.tsx
+++ components/resume.tsx
@@ -2,20 +2,34 @@
 
 import Button from "./button";
 import { Download } from "lucide-react";
+import { motion } from "framer-motion";
 export default function Resume() {
-
-  
   return (
-    <Button className="text-white bg-black" onClick={() => {
-        let a = document.createElement("a");
-        a.href = "/resume/JoshuaRodrigues_resume_2023.pdf";
-        a.target = "_blank";
-        a.click();
+    <motion.div
+      animate={{
+        y: [1, 4, -4, 1],
+      }}
+      transition={{
+        duration: 0.5,
+        ease: "easeInOut",
 
-    }}>
-      <span className="flex text-lg font-bolder">
-        Resume <Download className={`ml-2 `} />{" "}
-      </span>
-    </Button>
+        repeat: Infinity,
+        repeatDelay: 1,
+      }}
+    >
+      <Button
+        className="text-white bg-black"
+        onClick={() => {
+          let a = document.createElement("a");
+          a.href = "/resume/JoshuaRodrigues_resume_2023.pdf";
+          a.target = "_blank";
+          a.click();
+        }}
+      >
+        <span className="flex text-lg font-bolder">
+          Resume <Download className={`ml-2 `} />{" "}
+        </span>
+      </Button>
+    </motion.div>
   );
 }
