8cac69958250bc0ec488b4393e6976473b29c440
Joshua <rodrigues.joshr@gmail.com>
2023-12-22 23:18:03 +0530

    about update

--- components/about.tsx
+++ components/about.tsx
@@ -6,34 +6,14 @@ const About = () => {
   return (
     <div>
       <section className="w-full flex flex-wrap">
-        <Tags>Problem Solver</Tags>
-        <Tags>Bookworm</Tags>
-        <Tags>Cinema enthusiast</Tags>
+
+        <Tags>Bookworm, currently reading Prophet Song by Paul Lynch</Tags>
+        <Tags>Cinema enthusiast, current favourite The Mist</Tags>
+        <Tags>Loves to cook</Tags>
+        <Tags>Daily evening runs</Tags>
         <Tags>Cat and dog person</Tags>
       </section>
-      <p className="p-2">
-        Joshua considers himself a problem solver, with many of his projects
-        stemming from a genuine desire to find solutions to challenges he has
-        personally encountered.
-      </p>
-      <p className="p-2">
-        Beyond the realm of software development, Joshua has a deep appreciation
-        for cinema, particularly relishing the discovery and enjoyment of rare
-        and classic films from around the world. His viewing preferences lean
-        towards works from renowned directors such as Akira Kurosawa and Wong
-        Kar-wai.
-      </p>
-      <p className="p-2">
-        Devoting a significant amount of his time to reading, Joshua thoroughly
-        enjoys this pastime, exploring genres like thrillers, science fiction,
-        and classic literature. Currently, he is engrossed in the literary
-        masterpiece, The Brothers Karamazov.
-      </p>
-      <p className="p-2">
-        In addition to his professional and intellectual pursuits, Joshua has a
-        soft spot for cats 😸, although he is a proud owner of a dog 🐶 whom he
-        also holds in high regard.
-      </p>
+  
     </div>
   );
 };
