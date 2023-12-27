52df650ca075a4ee5f61547f24334f55bac139e0
Joshua <rodrigues.joshr@gmail.com>
2023-12-27 16:53:02 +0530

    minor adjustment

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -25,10 +25,10 @@ export default function ProjectCard({
     <>
       <Link  href={link}>
         <figure
-          className={`w-full sm:w-[250px] md:w-96 lg:w-full md:h-64 lg:h-72 overflow-hidden rounded-md border-2 border-black ${theme} font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]`}
+          className={`w-full h-44 sm:w-[250px] md:w-96 lg:w-full md:h-64 lg:h-72 overflow-hidden rounded-md border-2 border-black ${theme} font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]`}
         >
           <Image
-            className="w-full h-24 md:h-48  lg:h-56  lg:w-full"
+            className="w-full h-28 md:h-48  lg:h-56  lg:w-full"
             width={1000}
             height={1000}
             src={imageUrl}
