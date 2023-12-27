b6c84c2f9dc918018db493f9e3a0fd060f1ad3fa
Joshua <rodrigues.joshr@gmail.com>
2023-12-27 13:57:40 +0530

    layout done for lg screen

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,19 +1,19 @@
-"use client";
 
-import { useTheme } from "@/store/theme";
+
+
 import Tags from "./tags";
 
 const Intro = () => {
-  const theme = useTheme((s) => s.theme);
+
   return (
-    <div className=" font-bold md:font-extrabold ">
+    <div className="w-full flex flex-col items-center justify-center font-bold lg:pt-5 md:font-extrabold ">
       <section className="flex flex-col items-center justify-center">
         <Tags>
-          <h2 className="text-2xl md:text-3xl text-center">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl lg:font-bold text-center ">
             Building web solutions to everyday problems.
           </h2>
         </Tags>
-        <h2 className="text-xl md:text-2xl font-medium text-center pt-2">
+        <h2 className="text-xl md:text-2xl lg:text-3xl font-medium text-center pt-2 lg:pt-5 lg:mb-2">
           Joshua is specializing in building exceptional digital experiences to make everyday life easier.
         </h2>
       </section>
