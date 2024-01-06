bae852223758ec6645a8c6cd6be550354c1a6fda
Joshua <rodrigues.joshr@gmail.com>
2024-01-06 16:57:19 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
@@ -28,6 +29,7 @@ export default function RootLayout({
           </section>
           {children}
           <SpeedInsights />
+          <Analytics />
         </main>
       </body>
     </html>
